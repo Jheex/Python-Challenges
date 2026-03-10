f2e3ab7a87a0ad22bad62dd342c3106e95cc1fbb
Jhonatan Lima <Jhonatanlimacoelho25@gmail.com>
2026-03-10 14:56:38 -0300

    Desafio 30

--- Desafio_30/Desafio.py
+++ Desafio_30/Desafio.py
@@ -1,1 +1,22 @@
-#Exercicio 4
+print("")
+print("-----Carrinho de Compras-----")
+
+carrinho = []
+
+print("")
+r1 = input("Digite o primeiro item: ")
+carrinho.append(r1)
+print("Item inserido com sucesso!")
+print("")
+
+r2 = input("Digite o segundo item: ")
+carrinho.append(r2)
+print("Item inserido com sucesso!")
+print("")
+
+r3 = input("Digite o terceiro item: ")
+carrinho.append(r3)
+print("Item inserido com sucesso!")
+print("")
+
+print(f"Sua lista de compras é: {carrinho}")
