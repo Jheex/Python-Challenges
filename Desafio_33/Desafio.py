ec807662c61e5b0b5973566ec12f81e5cab89915
Jhonatan Lima <Jhonatanlimacoelho25@gmail.com>
2026-03-12 11:40:09 -0300

    Desafio 33

---
+++ Desafio_33/Desafio.py
@@ -0,0 +1,10 @@
+def calcular ():
+    n1 = (float(input("Digite o primeiro número: ")))
+    n2 = (float(input("Digite o segundo número: ")))
+
+    soma = n1 + n2
+    subtracao = n1 - n2
+    print("Soma:", soma)
+    print("subtração: ", subtracao)
+
+calcular()
