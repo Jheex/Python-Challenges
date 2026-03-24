9e7365a1785c14fbd065991cf23802d6614fd5c7
Jhonatan Lima <Jhonatanlimacoelho25@gmail.com>
2026-03-24 12:04:28 -0300

    Aprendendo MAX e MIN

--- Desafio_37/desafio.py
+++ Desafio_37/desafio.py
@@ -1,1 +1,13 @@
-# Desafio_37
+lista = []
+
+for i in range (0,5):
+    resposta = int(input("Digite um número: "))
+    lista.append(resposta)
+
+print("Lista: ",lista)
+
+maior = max(lista)
+menor = min(lista)
+
+print(f"O maior é {maior}")
+print(f"O menor é {menor}")
