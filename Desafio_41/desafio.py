d3475989453ed04c8265e5836aaa9dfbeb29d6ef
Jhonatan Lima <Jhonatanlimacoelho25@gmail.com>
2026-03-24 14:05:18 -0300

    Desafio 31

---
+++ Desafio_41/desafio.py
@@ -0,0 +1,9 @@
+# Desafio_41
+
+filme = {
+    "titulo": "Procurando nemo",
+    "ano": 2015,
+    "genero": "infantil"
+}
+
+print(f"O filme {filme["titulo"]} é do gênero {filme["genero"]} e foi lançado em {filme["ano"]}.")
