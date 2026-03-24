6c001f28606892bbd2e5d02dc08a03927244e0c3
Jhonatan Lima <Jhonatanlimacoelho25@gmail.com>
2026-03-24 14:02:17 -0300

    Dicionarios

---
+++ Desafio_40/desafio.py
@@ -0,0 +1,23 @@
+# Desafio_40
+
+'''
+Agora você vai:
+
+Criar um dicionário chamado carro
+Ele deve ter:
+marca
+modelo
+ano
+Depois exiba tudo assim (sem usar print separado):
+A marca do carro é X
+O modelo do carro é Y
+O ano do carro é Z
+'''
+
+carro = {
+    "marca": "chevrolet",
+    "modelo": "Celta",
+    "ano": 2002
+}
+
+print(f"A marca do carro é {carro['marca']}, O modelo do carro é {carro['modelo']}, O ano do carro é {carro['Ano']}")
