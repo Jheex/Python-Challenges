22f01c3332280aaad788cb96205a3aeb6cdb14a1
Jhonatan Lima <Jhonatanlimacoelho25@gmail.com>
2026-03-24 12:48:04 -0300

    Dicionarios

---
+++ Desafio_39/desafio.py
@@ -0,0 +1,21 @@
+'''Crie um dicionário chamado aluno que contenha:
+
+nome de um aluno
+idade
+nota final
+
+E depois exiba:
+
+o nome do aluno
+a idade
+a nota'''
+
+aluno = {
+    "nome": "Jhonatan",
+    "idade": 20,
+    "nota": 10
+}
+
+print(aluno["nome"])
+print(aluno["idade"])
+print(aluno["nota"])
