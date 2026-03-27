657ba928cd2e1f5c72c0f03d6bcce6f2c1185682
Jhonatan Lima <Jhonatanlimacoelho25@gmail.com>
2026-03-27 10:02:30 -0300

    Desafio 52

---
+++ Desafio_52/desafio.py
@@ -0,0 +1,12 @@
+# Desafio_52
+while True:
+    print("")
+    resposta = input("Digite uma frase: ")
+
+    Caracteres = len(resposta)
+    espacos = resposta.count(" ")
+
+    print("-----------------------------------------")
+    print("Caracteres: ", Caracteres)
+    print("Espaços: ", espacos)
+    print("-----------------------------------------")
