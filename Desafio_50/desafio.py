5df3ff8363db1d27bfaacaddf6adc2d54c945161
Jhonatan Lima <Jhonatanlimacoelho25@gmail.com>
2026-03-27 09:31:05 -0300

    Aprendendo len

--- Desafio_50/desafio.py
+++ Desafio_50/desafio.py
@@ -1,42 +1,6 @@
-import json
-
-def adicionar_tarefas():
-    tarefa = input("Qual tarefa deseja adicionar: ")
-    
-    with open ("Desafio_50/tarefas.json", "r") as arquivo:
-        dados = json.load(arquivo)
-
-    dados.append(tarefa)
-
-    with open ("Desafio_50/tarefas.json", "w") as arquivo:
-        json.dump(dados, arquivo, indent=4)
-
-    print(f"Lista: {dados}")
-
-def listar_tarefas():
-    with open ("Desafio_50/tarefas.json", "r") as arquivo:
-        dados = json.load(arquivo)
-
-    for i in (0, dados):
-        print(f"{i} - {dados}") 
-
-
-
+# Exemplo de instrução:
+# Escreva uma palavra e descubra quantas letras tem usando len()
 
 while True:
-    print("")
-    print("--------------------------")
-    print("    SISTEMA DE TAREFAS"    )
-    print("--------------------------")
-    print("1 - Adicionar tarefa"      )
-    print("2 - Listar tarefas"        )
-    print("3 - Remover tarefa"        )
-    print("4 - Sair"                  )
-    print("--------------------------")
-    opcao = int(input("Selecione uma opção: "))
-    print("")
-
-    if opcao == 1:
-        adicionar_tarefas()
-    elif opcao == 2:
-        listar_tarefas()
+    escrita = input("Digite algo: ")
+    print(len(escrita))
