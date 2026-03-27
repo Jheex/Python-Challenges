5532b3d12d71272e7cb557f79b2fd66c359b866a
Jhonatan Lima <Jhonatanlimacoelho25@gmail.com>
2026-03-27 10:17:43 -0300

    Desafio 54

---
+++ Desafio_54/desafio.py
@@ -0,0 +1,15 @@
+# Desafio_54
+
+"""
+Peça ao usuário para digitar uma frase e mostre quantas palavras tem.
+
+💡 Dica:
+
+len(frase.split())
+"""
+
+frase = input("Digite uma frase: ")
+
+palavras = len(frase.split())
+
+print("Quantidade de palavras: ",palavras)
