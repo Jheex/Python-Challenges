5bcfefaa2c7a6b282fc2db0202bcaa81c0fead2b
Jhonatan Lima <Jhonatanlimacoelho25@gmail.com>
2026-03-30 11:03:00 -0300

    Desafio 60

---
+++ Desafio_60/desafio.py
@@ -0,0 +1,14 @@
+# Desafio_60
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def apresentar(self):
+        print(f"Olá, meu nome é {self.nome} e eu tenho {self.idade} anos.")
+
+p1 = Pessoa("Jhonatan", 20)
+p2 = Pessoa("Jasmine", 29)
+p1.apresentar()
+p2.apresentar()
